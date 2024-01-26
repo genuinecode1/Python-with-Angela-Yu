8b7231333249c62f179d313b9eb8e7b71c65f595
genuine_code <113706866+genuinecode1@users.noreply.github.com>
2024-01-26 10:36:50 +0530

    Add files via upload

---
+++ BandNameGenerator.py
@@ -0,0 +1,14 @@
+# greeting message
+print("Welcome to Band Name Generator")
+
+# variable for city 
+city = input("Which city did you grow up in?\n")
+
+# variable for pet name
+pet = input("what is your pet name?\n")
+
+# suggestion for band name
+print("Your Band name could be: '" + city + " " +pet + "' or '" + pet + " " + city+"'")
+
+# thankyou message
+print("Thank You for using our services")
